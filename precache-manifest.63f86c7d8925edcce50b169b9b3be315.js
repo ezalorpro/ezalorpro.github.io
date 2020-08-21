8aaad89efd08b75ca62fb7a44689f6b0da543de6
kleiver <kleiver615@gmail.com>
2020-08-20 23:49:20 -0400

    pwa bosquejo

---
+++ precache-manifest.63f86c7d8925edcce50b169b9b3be315.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b57d4af169347d8faf8a",
+    "url": "/css/app.5fef8457.css"
+  },
+  {
+    "revision": "9af176d46a8e9cb8e61c0a4d858d7a89",
+    "url": "/images/cropped-Avonce_Logo_header-4.png"
+  },
+  {
+    "revision": "fd59d4acec910d4bccf6103cf47efd49",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b57d4af169347d8faf8a",
+    "url": "/js/app.7ee511f1.js"
+  },
+  {
+    "revision": "e676d98fb9dddd9b5617",
+    "url": "/js/chunk-vendors.8ad13e29.js"
+  },
+  {
+    "revision": "52c0507b1cc17a713ed5dfe1371b0395",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
---
+++ precache-manifest.dfc361555e47d8fed9f90862791248bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3ef303e1b0ae9f44a1fc",
+    "url": "/css/app.5fef8457.css"
+  },
+  {
+    "revision": "7fc24e21a7a3e41d4db58e882e457d7f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ef303e1b0ae9f44a1fc",
+    "url": "/js/app-legacy.d13d5795.js"
+  },
+  {
+    "revision": "f56b1e0a9caaf67d9eef",
+    "url": "/js/chunk-vendors-legacy.48dee10c.js"
+  },
+  {
+    "revision": "52c0507b1cc17a713ed5dfe1371b0395",
+    "url": "/manifest.json"
+  }
+]);
