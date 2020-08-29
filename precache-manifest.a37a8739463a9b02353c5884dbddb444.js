6824395b4baf22328778010a411990bc09a7908e
kleiver <kleiver615@gmail.com>
2020-08-29 17:02:06 -0400

    testing icons

---
+++ precache-manifest.a37a8739463a9b02353c5884dbddb444.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d38a2b58df330c85e0029eecf71d7c26",
+    "url": "/_redirects"
+  },
+  {
+    "revision": "5248ba84db910863fc26",
+    "url": "/css/app.aea48f19.css"
+  },
+  {
+    "revision": "9af176d46a8e9cb8e61c0a4d858d7a89",
+    "url": "/images/cropped-Avonce_Logo_header-4.png"
+  },
+  {
+    "revision": "c95e85bbe0307eb53fdd4d416843e99b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5248ba84db910863fc26",
+    "url": "/js/app.6dd44386.js"
+  },
+  {
+    "revision": "07e20e884415efb2c269",
+    "url": "/js/chunk-vendors.e41f77cb.js"
+  },
+  {
+    "revision": "6a14adf67f991c529dc9c9a417849079",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
